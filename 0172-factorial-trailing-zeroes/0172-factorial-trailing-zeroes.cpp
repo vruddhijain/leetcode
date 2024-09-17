15af6b512e1a39b58c04dac0d03e22c38dfb6891
Vruddhi Jain <117644309+vruddhijain@users.noreply.github.com>
2024-09-17 12:14:11 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (92.81%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+   
+    
+    int trailingZeroes(int n) {
+        
+        int fives = 0;
+        for (int i = 5; i <= n; i *= 5) {
+            fives += n / i;
+        }
+        return fives;
+        
+        
+    }
+};
