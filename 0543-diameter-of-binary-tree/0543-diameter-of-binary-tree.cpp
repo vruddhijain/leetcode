efbf50e7de093377613628a47e1d56609e3ea133
Vruddhi Jain <117644309+vruddhijain@users.noreply.github.com>
2024-09-23 20:30:06 +0530

    Time: 3 ms (99.34%), Space: 22.4 MB (58.49%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,24 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int re(TreeNode* root, vector<int>& res) {
-        if (root == NULL) {
-            return 0;  // Base case: If node is NULL, return 0 depth
+    
+    int ma = 0;
+    int re(TreeNode* root){
+        if(root == NULL){
+            return 0;
         }
         
-        int ldepth = re(root->left, res);  // Recursively find left depth
-        int rdepth = re(root->right, res); // Recursively find right depth
-
-        // The diameter at the current node is the sum of left and right depths
-        int diameter = ldepth + rdepth;
-        res.push_back(diameter);  // Store the diameter in the result vector
         
-        // Return the height of the current node
-        return 1 + max(ldepth, rdepth);
+        int left = re(root->left);
+        int right = re(root->right);
+        
+        int dep = max(left, right) + 1;
+        
+        ma = max(ma, left+right);
+        
+        return dep;
+        
+        
+        
     }
-
     int diameterOfBinaryTree(TreeNode* root) {
-        vector<int> res;
-        re(root, res);
-        return *max_element(res.begin(), res.end()); // Return the maximum diameter found
+        
+        int r = re(root);
+        return ma;
+        
+        
     }
 };
