7b4e27a76f05e8357eb868814a0ac353d0a855a0
Vruddhi Jain <117644309+vruddhijain@users.noreply.github.com>
2024-09-25 21:49:51 +0530

    Time: 3 ms (52.27%), Space: 11.6 MB (32.44%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        
+    vector<string> x;
+        
+    for(int i =1; i<=n; i++){
+        if(i % 3 ==0 && i % 5 == 0){
+            x.push_back("FizzBuzz");
+        }
+        
+        else if(i % 3 == 0 && i % 5 != 0){
+            x.push_back("Fizz");
+        }
+        
+        else if(i % 3 != 0 && i % 5 == 0){
+            x.push_back("Buzz");
+        }
+        
+        else{
+            string c = to_string(i);
+            x.push_back(c);
+        }
+        
+        
+        
+    }
+        
+        return x;
+        
+        
+        
+    }
+};
