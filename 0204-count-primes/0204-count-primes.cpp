8cee2322b29d0bb953a8b8b91998e4d15a199108
Vruddhi Jain <117644309+vruddhijain@users.noreply.github.com>
2024-10-05 20:07:10 +0530

    Time: 417 ms (62.12%), Space: 11.8 MB (41.85%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -6,15 +6,21 @@ public:
        vector<bool>prime(n+1,true);
         prime[0]=prime[1]=false;
         int c=0;
-        for(int i=2;i<n;i++){
+        for(int i=2;i<sqrt(n);i++){
             if(prime[i]){
-                c++;
-                for(int j=2*i;j<n;j=j+i){
+                
+                for(int j=i*i;j<n;j=j+i){
                     prime[j]=0;
                 }
             }
 
         }
+        for(int i=0; i<n; i++){
+            if(prime[i]){
+                c++;
+            }
+        }
+        
         return c;
         
     }
